ae615b581fe270d86afe254ffeadf67232d0361a
enarroied <eric.narro.ied@gmail.com>
2025-10-11 22:53:03 +0200

    fixed typos in UI

--- src/page/page.py
+++ src/page/page.py
@@ -1,7 +1,7 @@
 import taipy.gui.builder as tgb
 
 with tgb.Page() as page:
-    tgb.text("# Wine production by Region and Year", mode="md")
+    tgb.text("# Wine Production by Region and Year", mode="md")
     tgb.html("hr")
-    tgb.text("## Data for all the regions:", mode="md")
+    tgb.text("## Data for All Regions:", mode="md")
     tgb.table("{df_wine_production}")
