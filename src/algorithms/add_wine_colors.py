f26063fa7e58c7d2a6245872f2e7c96a5c61ed26
enarroied <eric.narro.ied@gmail.com>
2025-10-11 23:01:52 +0200

    cleaned and optimized add_wine_colors

--- src/algorithms/add_wine_colors.py
+++ src/algorithms/add_wine_colors.py
@@ -1,40 +1,32 @@
+def _detect_wine_color(wine_type, aoc, color_keyword):
+    """
+    Helper: Detects if a wine matches a given color (e.g., 'ROUGE' or 'BLANC'),
+    accounting for special cases like 'NORD-EST' wines where color info
+    appears in the AOC name.
+    """
+    return (
+        wine_type.str.contains(color_keyword, regex=False)
+        | (
+            aoc.str.contains(color_keyword, regex=False)
+            & wine_type.str.contains("NORD - EST", regex=False)
+        )
+    ).astype(int)
+
+
 def add_wine_colors(df_wine):
-    """Adds 2 columns with
+    """Adds 2 columns with 1/0 values for red/rose color, and white color.
+    `red_and_rose` and `white` columns are incompatible (both can't be 1).
     Args:
         df_wine (DataFrame): Data from the csv file (input for the whole app)
 
     Returns:
-        df_wine_with_colors: DataFrame with all the input columns plus 2 new
-    ones, 'red_and_rose' and 'white', and drops 2 columns that are not needed
+        df_wine_with_colors: DataFrame
     """
-    print("add wine color columns")
-    df_wine_with_colors = df_wine.reset_index(drop=True)
+    wine_with_colors = df_wine.copy().reset_index(drop=True)
 
-    df_wine_with_colors["red_and_rose"] = 0
-    df_wine_with_colors["white"] = 0
+    wine_type = wine_with_colors["wine_type"].str.upper().fillna("")
+    aoc = wine_with_colors["AOC"].str.upper().fillna("")
 
-    # General case:
-    df_wine_with_colors.loc[
-        df_wine_with_colors["wine_type"].str.contains("ROUGE"), "red_and_rose"
-    ] = 1
-    df_wine_with_colors.loc[
-        df_wine_with_colors["wine_type"].str.contains("BLANC"), "white"
-    ] = 1
-
-    # For some reason, there is some lines where the information about
-    # wine color is in the name:
-    df_wine_with_colors.loc[
-        df_wine_with_colors["AOC"].str.contains("Rouge")
-        & df_wine_with_colors["wine_type"].str.contains("NORD - EST"),
-        "red_and_rose",
-    ] = 1
-    df_wine_with_colors.loc[
-        df_wine_with_colors["AOC"].str.contains("Blanc")
-        & df_wine_with_colors["wine_type"].str.contains("NORD - EST"),
-        "white",
-    ] = 1
-
-    # Droip unnecessary columns
-    df_wine_with_colors = df_wine_with_colors.drop(columns=["data_type", "wine_type"])
-
-    return df_wine_with_colors
+    wine_with_colors["red_and_rose"] = _detect_wine_color(wine_type, aoc, "ROUGE")
+    wine_with_colors["white"] = _detect_wine_color(wine_type, aoc, "BLANC")
+    return wine_with_colors.drop(columns=["data_type", "wine_type"])
